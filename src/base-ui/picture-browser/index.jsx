9d27913293ba9319d3b226032351e10da97d9539
duyuanqiang <495579962@qq.com>
2022-10-18 01:13:37 +0800

    添加react动画

--- src/base-ui/picture-browser/index.jsx
+++ src/base-ui/picture-browser/index.jsx
@@ -1,15 +1,18 @@
+import PropTypes from 'prop-types'
+import React, { memo, useCallback, useState } from 'react'
+import {SwitchTransition,CSSTransition} from "react-transition-group"
 import IconArrowLeft from '@/assets/svg/icon-arrow-left'
 import IconArrowRight from '@/assets/svg/icon-arrow-right'
 import IconClose from '@/assets/svg/icon-close'
 import classNames from 'classnames'
-import PropTypes from 'prop-types'
-import React, { memo, useCallback, useState } from 'react'
 import Indicator from '../indicator'
 import { BrowserWrapper } from './style'
 
 const PictureBrowser = memo((props) => {
   const {picturesData,clickHandle} = props
   const [currentIndex,setCurrentIndex] = useState(0)
+  const [isShowList,setIsShowList] = useState(true)
+  const [isNext,setIsNext] = useState(true)
 
   function closeClickHandle() {
     clickHandle()
@@ -19,44 +22,63 @@ const PictureBrowser = memo((props) => {
     let index = isRight ? currentIndex + 1 : currentIndex - 1;
     if(index < 0 ) index = picturesData.length - 1
     if(index >= picturesData.length) index = 0
-    console.log(index,e);
     setCurrentIndex(index)
+    setIsNext(isRight)
   },[currentIndex,picturesData.length])
 
+  function isIsShowList() {
+    setIsShowList(!isShowList)
+  }
+
   return (
-    <BrowserWrapper >
+    <BrowserWrapper isNext={isNext} showList={isShowList}>
       <div className="content">
-        <div className="close" onClick={closeClickHandle}>
+        <div className="top" onClick={closeClickHandle}>
+          <div className='close'>
             <IconClose/>
+          </div>
         </div>
         <div className="main">
-          <div className='main-pic'>
             <div className="left" onClick={e=>arrowClickHandle(false,e)}>
               <IconArrowLeft width={80} height={80}/>
             </div>
             <div className="img">
-              <img src={picturesData[currentIndex]} alt="" />
+              <SwitchTransition mode='out-in'>
+                <CSSTransition key={picturesData[currentIndex]} timeout={200} classNames="pic">
+                    <img src={picturesData[currentIndex]} alt="" />
+                </CSSTransition>
+              </SwitchTransition>
             </div>
             <div className="right" onClick={e=>arrowClickHandle(true,e)}>
               <IconArrowRight width={80} height={80}/>
             </div>
-          </div>
-          <div className="idcator">
+        </div>
+        <div className="preview">
+          <div className="info">
             <div className="desc">
-              <span>2/12：Lovely smart open-plan living space with a corner sofa and accent chair, wall-mounted 43 inch Smart TV, dining table and large windows overlooking Fitzrovia</span>
+              <div className="caption">
+                <span>
+                  2/12：Lovely smart open-plan living space with a corner sofa and accent chair, 
+                  wall-mounted 43 inch Smart TV, dining table and large windows overlooking Fitzrovia
+                </span>
+              </div>
+              <div className="hide" onClick={isIsShowList}>
+                <span>{isShowList? "显示":"隐藏"}图片列表</span>
+              </div>
             </div>
-            <Indicator selectIndex={currentIndex}>
-              {
-                picturesData.map((item,index)=>{
-                  return (
-                    <div className={classNames("item",{active:currentIndex===index})} key={item}>
-                      <img src={item} alt="" />
-                    </div>
-                    // <div className="cover"></div>
-                  )
-                })
-              }
-            </Indicator>
+            <div className="indicator">
+              <Indicator selectIndex={currentIndex}>
+                {
+                  picturesData.map((item,index)=>{
+                    return (
+                      <div className={classNames("item",{active:currentIndex===index})} key={item}>
+                        <img src={item} alt="" />
+                      </div>
+                    )
+                  })
+                }
+              </Indicator>
+            </div>
           </div>
         </div>
         
--- src/base-ui/picture-browser/style.js
+++ src/base-ui/picture-browser/style.js
@@ -9,61 +9,97 @@ export const BrowserWrapper = styled.div`
   background: rgba(2,2,2,1);
   z-index: 999;
   .content{
-    position: relative;
+    display: flex;
+    flex-direction: column;
     width: 100%;
     height: 100%;
-    overflow: hidden;
-    .close{
-      position: absolute;
-      right: 0;
-      top: 0;
-      padding: 20px;
+    .top{
+      position: relative;
+      height: 86px;
+      .close{
+        position: absolute;
+        top: 20px;
+        right: 20px;
+      }
     }
     .main{
-      width: 100%;
-      height: 100%;
-      margin-top: 60px;
-      
-      .main-pic{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        color: #fff;
-        .left{
+      flex: 1;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      color: #fff;
+      .img{
+        position: relative;
+        overflow: hidden;
+        width: 100%;
+        height: 100%;
+        max-width: 105vh;
+        img{
+          /* position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          margin: 0 auto; */
           height: 100%;
+          /* user-select: none; */
         }
-        .img{
-          img{
-            height: 400px;
-            max-width: 100%;
-          }
+        /* 动画样式 */
+        .pic-enter{
+          transform: translateX${ props=>props.isNext ? "-100%" : "100%"};
+          opacity: 0;
+        }
+        .pic-enter-active{
+          transform: translate(0);
+          opacity: 1;
+          transition: all 200ms ease;
+        }
+        .pic-exit{
+          opacity: 1;
+        }
+        .pic-exit-active{
+          opacity: 0;
+          transition: all 200ms ease;
         }
       }
-      .idcator{
-        margin: 0 auto;
-        overflow: hidden;
-        width: 600px;
-        color: #fff;
+    }
+    .preview{
+      margin: 10px auto;
+      color: #fff;
+      .info{
+        /* position: absolute;
+        bottom: 10px; */
+        max-width: 105vh;
         .desc{
+          display: flex;
+          justify-content: space-between;
           margin-bottom: 10px;
-        }
-        .item{
-          overflow: hidden;
-          margin: 0 10px;
-          flex-shrink: 0;
-          width: 100px;
-          height: 300px;
-          img{
-            width: 100%;
-            object-fit: cover;
-            -webkit-filter: grayscale(80%); /* Chrome, Safari, Opera */
-            filter: grayscale(80%); 
+          .caption{
+
+          }
+          .hide{
+            cursor: pointer;
           }
         }
-        .active{
-          img{
-            -webkit-filter: none; /* Chrome, Safari, Opera */
-            filter: none;
+        .indicator{
+          overflow: hidden;
+          transition: height 0.2s ease;
+          height: ${props=>props.showList ? "67px" : "0"};
+          .item{
+            margin: 0 10px;
+            flex-shrink: 0;
+            cursor: pointer;
+            img{
+              height: 67px;
+              object-fit: cover;
+              -webkit-filter: grayscale(80%); /* Chrome, Safari, Opera */
+              filter: grayscale(80%); 
+            }
+            &.active{
+              img{
+                -webkit-filter: none; /* Chrome, Safari, Opera */
+                filter: none;
+              }
+            }
           }
         }
       }
